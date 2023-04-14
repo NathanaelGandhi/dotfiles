fb40d42bf0c552addbd4bdfc9cea8d1cde2c5fc5
Nathanael Gandhi <nat11public@gmail.com>
2023-04-14 21:08:01 +1000

    feat(install) link direnv apt

--- install-apts.sh
+++ install-apts.sh
@@ -9,6 +9,22 @@ sudo apt update && sudo apt upgrade -y
 echo "[install-apts] installing apts"
 sudo apt install -y terminator zsh fonts-powerline flatpak gnome-software-plugin-flatpak cpu-x photocollage neofetch git podman distrobox clang-format direnv
 
+## LINK APTS BASH
+if grep -Fxq 'eval "$(direnv hook bash)"' ~/.bashrc
+then
+    echo "direnv bash hook already exists"
+else
+    echo 'eval "$(direnv hook bash)"' | tee -a ~/.bashrc
+fi
+
+## LINK APTS ZSH
+if grep -Fxq 'eval "$(direnv hook zsh)"' ~/.zshrc
+then
+    echo "direnv zsh hook already exists"
+else
+    echo 'eval "$(direnv hook zsh)"' | tee -a ~/.zshrc
+fi
+
 ## COMPLETE
 echo "[install-apts]"
 echo "--- COMPLETE ---"
